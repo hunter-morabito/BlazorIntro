68bd1624d782ac9c9570f4d56a2b313635c03652
Hunter Morabito <hunter.morabito@gmail.com>
2022-08-21 14:08:46 -0400

    Added EditEmployee Form

---
+++ BethanysPieShopHRM.Server/Pages/EmployeeEdit.cs
@@ -0,0 +1,22 @@
+﻿using BethanysPieShopHRM.Server.Services;
+using BethanysPieShopHRM.Shared;
+using Microsoft.AspNetCore.Components;
+
+namespace BethanysPieShopHRM.Server.Pages
+{
+    public partial class EmployeeEdit
+    {
+        [Inject]
+        public IEmployeeDataService EmployeeDataService { get; set; }
+
+        [Parameter]
+        public string EmployeeId { get; set; }
+
+        public Employee Employee { get; set; } = new ();
+
+        protected override async Task OnInitializedAsync()
+        {
+            Employee = await EmployeeDataService.GetEmployeeDetails(int.Parse(EmployeeId));
+        }
+    }
+}
