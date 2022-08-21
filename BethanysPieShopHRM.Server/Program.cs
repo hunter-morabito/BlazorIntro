3fd66e0c3877cbaa10c79fe4c24d3c2b3bfea0ce
Hunter Morabito <hunter.morabito@gmail.com>
2022-08-21 13:48:21 -0400

    Accessing Data from API

--- BethanysPieShopHRM.Server/Program.cs
+++ BethanysPieShopHRM.Server/Program.cs
@@ -1,4 +1,5 @@
 using BethanysPieShopHRM.Server;
+using BethanysPieShopHRM.Server.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -6,6 +7,6 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddHttpClient<IEmployeeDataService, EmployeeDataService>(client => client.BaseAddress = new Uri("https://localhost:44340/"));
 
 await builder.Build().RunAsync();
